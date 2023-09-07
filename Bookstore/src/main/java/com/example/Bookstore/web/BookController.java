0fd660775126bc32ab2897985d44548eeea1923e
hxrda <henein.roda@gmail.com>
2023-09-07 17:14:27 +0300

    Chapter 3 additions

--- Bookstore/src/main/java/com/example/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/com/example/Bookstore/web/BookController.java
@@ -1,15 +1,53 @@
 package com.example.Bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.Bookstore.model.Book;
+import com.example.Bookstore.model.BookRepository;
+
 @Controller
 public class BookController {
+	@Autowired
+	private BookRepository repository;
 
-	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String tba() {
-		return "tba";
+	// List of books (front page):
+	@RequestMapping(value = { "/", "/booklist" })
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
 	}
 
+	// Form to add books:
+	@RequestMapping(value = "/add")
+	public String addForm(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+
+	// Save functionality for form (the endpoint where the form(s) will be
+	// submitted):
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+
+	// Delete book (from front page):
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteStudent(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
+
+	// Form to edit books:
+	@RequestMapping(value = "/edit/{id}")
+	public String editForm(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("book", repository.findById(bookId));
+		return "editbook";
+	}
 }
