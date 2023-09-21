f461757dc478e52ed94ec63cfd7d526489d11843
hxrda <henein.roda@gmail.com>
2023-09-21 15:20:33 +0300

    added RESTful services

--- Bookstore/src/main/java/com/example/Bookstore/model/Category.java
+++ Bookstore/src/main/java/com/example/Bookstore/model/Category.java
@@ -2,6 +2,8 @@ package com.example.Bookstore.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 // Alternatively: import jakarta.persistence.*;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -18,6 +20,7 @@ public class Category {
 	private Long categoryId;
 	private String name;
 
+	@JsonIgnore
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
 	private List<Book> books;  //List: because department can have many students
 
--- Bookstore/src/main/java/com/example/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/com/example/Bookstore/web/BookController.java
@@ -1,32 +1,51 @@
 package com.example.Bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.Bookstore.model.Book;
 import com.example.Bookstore.model.BookRepository;
 import com.example.Bookstore.model.CategoryRepository;
 
+
+
 @Controller
 public class BookController {
+	
 	@Autowired
 	private BookRepository repository;
 	
 	@Autowired
 	private CategoryRepository crepository;
 
-	// List of books (front page):
+	// THYMELEAF: List of books (front page):
 	@RequestMapping(value = { "/", "/booklist" })
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
+	
+	//RESTful: get/list all books:
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();
+    } 
+	
+	//RESTful: get/list 1 student by ID:
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+    	return repository.findById(bookId);
+    }
 
-	// Form to add books:
+	// THYMELEAF: Form to add books:
 	@RequestMapping(value = "/add")
 	public String addForm(Model model) {
 		model.addAttribute("book", new Book());
@@ -34,7 +53,7 @@ public class BookController {
 		return "addbook";
 	}
 
-	// Save functionality for form (the endpoint where the form(s) will be
+	// THYMELEAF: Save functionality for form (the endpoint where the form(s) will be
 	// submitted):
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String save(Book book) {
@@ -42,14 +61,14 @@ public class BookController {
 		return "redirect:booklist";
 	}
 
-	// Delete book (from front page):
+	// THYMELEAF: Delete book (from front page):
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteStudent(@PathVariable("id") Long bookId, Model model) {
 		repository.deleteById(bookId);
 		return "redirect:../booklist";
 	}
 
-	// Form to edit books:
+	// THYMELEAF: Form to edit books:
 	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
 	public String editForm(@PathVariable("id") Long bookId, Model model) {
 		model.addAttribute("book", repository.findById(bookId));
